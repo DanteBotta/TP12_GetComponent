6f7c04595d5a48b5ddc3c11539695fcf6258b2f6
Damptermite4040 <dantegiovannibotta@gmail.com>
2026-05-05 14:25:47 -0300

    Initial commit

---
+++ Assets/Scripts/ColisionManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionManager : MonoBehaviour
+{
+    CubeSript cubeData;
+
+    void OnCollisionEnter(Collision col)
+    {
+        cubeData = col.gameObject.GetComponent<CubeSript>();
+        Debug.Log("Colisión con " + cubeData.cubename);
+        Debug.Log("Con valor de " + cubeData.value);
+    }
+}
---
+++ Assets/Scripts/CubeSript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSript : MonoBehaviour
+{
+    public string cubename;
+    public int value;
+}
